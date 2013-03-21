54da01b7631eb4940a960e5aa721e510df38efba
matthew kim <matthewjkim@gmail.com>
2013-03-21 04:12:31 +0000

    adding more examples

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,11 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+name = 'matt'
+template = ERB.new "my name is <%= name %>"
+puts template.result(binding)
+
+nums = [1,2,3,4]
+template = ERB.new "size of array is <%=nums.count%>"
+puts template.result(binding) 
